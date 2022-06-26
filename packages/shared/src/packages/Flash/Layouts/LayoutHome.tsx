a10a7ed89703409c0855d8e43a6e3dd2eebbff77
milky <listingslab@gmail.com>
2022-06-26 11:41:14 +0200

    Smarter Layout

--- packages/shared/src/packages/Flash/Layouts/LayoutHome.tsx
+++ packages/shared/src/packages/Flash/Layouts/LayoutHome.tsx
@@ -3,19 +3,23 @@ import { Grid} from '@mui/material'
 import { 
     RouteEngine,
     Work, Life, Balance,
+    Social,
 } from "../../../listingslab-shared"
 export default function LayoutHome() {
 
     return (
         <Grid container>
-            <Grid item sx={{ display: "flex"}}>
+            <Grid item xs={12} sx={{ display: "flex", mt:2}}>
                 <Work />
                 <Life />
                 <Balance />
             </Grid>
             <Grid item sx={{flexGrow: 1}} />
-            <Grid sx={{ border: "1px solid limegreen", }}>
+            <Grid item xs={ 12 } md={ 8 }>
                 <RouteEngine />
+            </Grid>
+            <Grid item xs={ 12 } md={ 4 } sx={{ mt:2}}>
+                <Social />
             </Grid>
             
         </Grid>
